8c34b1818142df7c4d63a445b355b612196b82fc
Icaro Souza <icarosouza04@outlook.com>
2025-11-15 16:50:38 -0300

    Finalizando o módulo de manipulação de arquivos

--- Aulas/04-Manipulando_arquivos.py
+++ Aulas/04-Manipulando_arquivos.py
@@ -1,46 +1,46 @@
 # Abrindo e fechando arquivos
 
-# file = open("exemplo.txt", "r") # Para ler um arquivo
+file = open("exemplo.txt", "r") # Para ler um arquivo
 
-# file = open("exemplo.txt", "w") # Para escrever um arquivo
+file = open("exemplo.txt", "w") # Para escrever um arquivo
 
-# file = open("exemplo.txt", "a") # Para anexar conteúdo a um arquivo existente
+file = open("exemplo.txt", "a") # Para anexar conteúdo a um arquivo existente
 
 # Lendo um arquivo
 
-# file = open("exemplo.txt", "r") # Para abrir o arquivo no modo leitura
-# print(file.read()) # Para ler o arquivo e exibir
-# file.close() # Para fechar o arquivo
+file = open("exemplo.txt", "r") # Para abrir o arquivo no modo leitura
+print(file.read()) # Para ler o arquivo e exibir
+file.close() # Para fechar o arquivo
 
-# arquivo = open(
-#     r"C:\Users\Icaro Souza\Documents\Local_GitHub\Python-Back-end-Luizalabs-Bootcamp\Aulas\Meus_jogos_favoritos.txt",
-#     "r")
+arquivo = open(
+    r"C:\Users\Icaro Souza\Documents\Local_GitHub\Python-Back-end-Luizalabs-Bootcamp\Aulas\Meus_jogos_favoritos.txt",
+    "r")
 
-# print(arquivo.read()) # Retorna o conteúdo do arquivo inteiro
-# print(arquivo.readline()) # Retorna uma linha do arquivo por vez
-# print(arquivo.readlines()) # Retorna uma lista com todas as linhas do arquivo
+print(arquivo.read()) # Retorna o conteúdo do arquivo inteiro
+print(arquivo.readline()) # Retorna uma linha do arquivo por vez
+print(arquivo.readlines()) # Retorna uma lista com todas as linhas do arquivo
 
-# while len(linha := arquivo.readline()): # Enquanto o tamanho da linha for maior que 0, atribui o conteúdo para a variável linha e exibe a mesma em loop
-#     print(linha)
+while len(linha := arquivo.readline()): # Enquanto o tamanho da linha for maior que 0, atribui o conteúdo para a variável linha e exibe a mesma em loop
+    print(linha)
 
-# arquivo.close()
+arquivo.close()
 
 # Escrevendo um arquivo
 
-# file = open("exemplo.txt", "w") # Para abrir o arquivo no modo de escrita
-# file.write("Olá, mundo!") # Para escrever algo no arquivo
-# file.close() # Para fechar o arquivo
+file = open("exemplo.txt", "w") # Para abrir o arquivo no modo de escrita
+file.write("Olá, mundo!") # Para escrever algo no arquivo
+file.close() # Para fechar o arquivo
 
-# arquivo = open(
-#     r"C:\Users\Icaro Souza\Documents\Local_GitHub\Python-Back-end-Luizalabs-Bootcamp\Aulas\Meus_personagens_favoritos.txt",
-#     "w"
-# )
+arquivo = open(
+    r"C:\Users\Icaro Souza\Documents\Local_GitHub\Python-Back-end-Luizalabs-Bootcamp\Aulas\Meus_personagens_favoritos.txt",
+    "w"
+)
 
-# arquivo.write("Ma Chao") # Escreve o conteúdo no arquivo (recomendado para textos grandes)
-# arquivo.writelines(["\n", "Keiji Maeda", "\n", "Yoshitsune Minamoto", "\n", "Naomasa Li", "\n", "Kuro", "\n",
-#                     "Godfrey", "\n", "Fera Clerical", "\n", "Kratos", "\n", "Kassandra", "\n", "Jin Sakai"])
-#                    # Escreve o conteúdo no arquivo (um por vez)
-# arquivo.close()
+arquivo.write("Ma Chao") # Escreve o conteúdo no arquivo (recomendado para textos grandes)
+arquivo.writelines(["\n", "Keiji Maeda", "\n", "Yoshitsune Minamoto", "\n", "Naomasa Li", "\n", "Kuro", "\n",
+                    "Godfrey", "\n", "Fera Clerical", "\n", "Kratos", "\n", "Kassandra", "\n", "Jin Sakai"])
+                   # Escreve o conteúdo no arquivo (um por vez)
+arquivo.close()
 
 # Gerenciando arquivos e diretórios
 
@@ -50,56 +50,56 @@ from pathlib import Path # Biblioteca para facilitar a identificação do caminh
 
 ROOT_PATH = Path(__file__).parent # __file__ representa o caminho do arquivo
 
-# os.mkdir("Exemplo") # Cria um diretório
+os.mkdir("Exemplo") # Cria um diretório
 
-# os.rename("Exemplo.txt", "Novo_exemplo.txt") # Renomeia o diretório
+os.rename("Exemplo.txt", "Novo_exemplo.txt") # Renomeia o diretório
 
-# os.remove("Novo_exemplo.txt") # Remove um arquivo
+os.remove("Novo_exemplo.txt") # Remove um arquivo
 
-# shutil.move("Exemplo.txt", "Novo_exemplo.txt") # Mover um arquivo
+shutil.move("Exemplo.txt", "Novo_exemplo.txt") # Mover um arquivo
 
-# os.mkdir(ROOT_PATH / "Arquivos")
+os.mkdir(ROOT_PATH / "Arquivos")
 
-# arquivo2 = open("Novo_arquivo.txt" "w")
-# arquivo2.close()
+arquivo2 = open("Novo_arquivo.txt" "w")
+arquivo2.close()
 
-# os.rename(ROOT_PATH / "Novo_arquivo.txt", ROOT_PATH / "Arquivo_definitivo.txt")
+os.rename(ROOT_PATH / "Novo_arquivo.txt", ROOT_PATH / "Arquivo_definitivo.txt")
 
-# os.remove(ROOT_PATH / "Arquivo_definitivo.txt")
+os.remove(ROOT_PATH / "Arquivo_definitivo.txt")
 
-# shutil.move(ROOT_PATH / "Meus_personagens_favoritos.txt", ROOT_PATH / "Arquivos" / "Meus_personagens_favoritos.txt")
+shutil.move(ROOT_PATH / "Meus_personagens_favoritos.txt", ROOT_PATH / "Arquivos" / "Meus_personagens_favoritos.txt")
 
 # Tratamento de exceções em manipulação de arquivos
 
-# try:
-#     arquivo = open(ROOT_PATH / "Arquivos")
-# except FileNotFoundError as exc:
-#     print(f"Este arquivo não existe. {exc}")
-# except IsADirectoryError as exc:
-#     print(f"Não foi possível encontrar o caminho deste arquivo. {exc}")
-# except IOError as exc:
-#     print(f"Erro ao abrir o arquivo. {exc}")
-# except Exception as exc:
-#     print(f"Algum problema ocorreu ao tentar abrir o arquivo. {exc}")
+try:
+    arquivo = open(ROOT_PATH / "Arquivos")
+except FileNotFoundError as exc:
+    print(f"Este arquivo não existe. {exc}")
+except IsADirectoryError as exc:
+    print(f"Não foi possível encontrar o caminho deste arquivo. {exc}")
+except IOError as exc:
+    print(f"Erro ao abrir o arquivo. {exc}")
+except Exception as exc:
+    print(f"Algum problema ocorreu ao tentar abrir o arquivo. {exc}")
 
 # Boas práticas na manipulação de arquivos
 
-# with open(ROOT_PATH / "Arquivos" / "Meus_personagens_favoritos.txt", "r") as arquivo:
-#     print(f"Lendo o arquivo: {arquivo}")
-#     print(arquivo.read())
+with open(ROOT_PATH / "Arquivos" / "Meus_personagens_favoritos.txt", "r") as arquivo:
+    print(f"Lendo o arquivo: {arquivo}")
+    print(arquivo.read())
 
-# try:
-#     with open(ROOT_PATH / "Arquivos" / "Meus_personagens_favoritos.txt", "r") as arquivo:
-#         print(f"Lendo o arquivo: {arquivo}")
-#         print(arquivo.read())
-# except IOError as erro:
-#     print(f"Erro ao abrir o arquivo.\n{erro}")
+try:
+    with open(ROOT_PATH / "Arquivos" / "Meus_personagens_favoritos.txt", "r") as arquivo:
+        print(f"Lendo o arquivo: {arquivo}")
+        print(arquivo.read())
+except IOError as erro:
+    print(f"Erro ao abrir o arquivo.\n{erro}")
 
-# try:
-#     with open(ROOT_PATH / "Arquivos" / "Arquivo_utf-8.txt", "w", encoding = "utf-8") as arquivo:
-#         arquivo.write(f"Aprendendo a manipular arquivos utilizando o Python.")
-# except IOError as erro:
-#     print(f"Erro ao abrir o arquivo.\n{erro}")
+try:
+    with open(ROOT_PATH / "Arquivos" / "Arquivo_utf-8.txt", "w", encoding = "utf-8") as arquivo:
+        arquivo.write(f"Aprendendo a manipular arquivos utilizando o Python.")
+except IOError as erro:
+    print(f"Erro ao abrir o arquivo.\n{erro}")
 
 try:
     with open(ROOT_PATH / "Arquivos" / "Arquivo_utf-8.txt", "r", encoding = "ascii") as arquivo:
@@ -108,3 +108,75 @@ except IOError as erro:
     print(f"Erro ao abrir o arquivo.\n{erro}")
 except UnicodeDecodeError as erro:
     print(f"Não foi possível abrir o arquivo. Há um caractere não compatível com o modelo de abertura.\n{erro}")
+
+# Trabalhando com arquivos `csv`
+
+import csv
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent # __file__ representa o caminho do arquivo
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for row in leitor:
+            print(row)
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for row in leitor:
+            print(row[0], row[1])
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
+
+COLUNA_ID = 0
+COLUNA_NOME = 1
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for row in leitor:
+            print(row[COLUNA_ID], row[COLUNA_NOME])
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for index, row in leitor:
+            if index == 0:
+               continue
+            print(row[COLUNA_ID], row[COLUNA_NOME])
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for index, row in leitor:
+            if index == 0:
+               continue
+            print(f"ID: {row[COLUNA_ID]}")
+            print(f"NOME: {row[COLUNA_NOME]}")
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.DictReader(arquivo)
+        for row in leitor:
+            print(row["ID"], row["NOME"])
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
+
+try:
+    with open(ROOT_PATH / "Arquivos" / "Alunos.csv", "r", newline = "", encoding = "utf-8") as arquivo:
+        leitor = csv.DictReader(arquivo)
+        for row in leitor:
+            print(f"ID: {row["ID"]}")
+            print(f"NOME: {row['NOME']}")
+except IOError as erro:
+    print(f"Erro ao criar arquivo.\n{erro}")
