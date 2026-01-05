5f28e00e11699d59bccb8b305f9ad3fa348047bd
Icaro Souza <icarosouza04@outlook.com>
2026-01-05 17:34:41 -0400

    Introdução ao desafio

---
+++ Aulas/APIs/FastAPI/Desafio/atleta/schemas.py
@@ -0,0 +1,13 @@
+from Aulas.APIs.FastAPI.Desafio.contrib.schemas import BaseSchema
+from pydantic import BaseModel, Field, PositiveFloat
+from typing import Annotated
+
+class Atleta(BaseSchema):
+    nome : Annotated[str, Field(description = "Nome do atleta", example = "Icaro", max_length = 50)]
+    cpf: Annotated[str, Field(description = "CPF do atleta", example = "00000000000", max_length = 11)]
+    idade: Annotated[int, Field(description = "Idade do atleta", example = "21")]
+    peso: Annotated[PositiveFloat, Field(description = "Peso do atleta", example = "70.1")]
+    altura: Annotated[PositiveFloat, Field(description = "Altura do atleta", example = "1.73")]
+    sexo: Annotated[str, Field(description = "Sexo do atleta", example = "M", max_length = 1)]
+
+
