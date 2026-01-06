15c70516232d8c558186758ec55ad428d70b109e
Icaro Souza <icarosouza04@outlook.com>
2026-01-06 14:25:01 -0400

    Docker e Alembic

---
+++ Aulas/APIs/FastAPI/Desafio/contrib/repository/models.py
@@ -0,0 +1,4 @@
+from Aulas.APIs.FastAPI.Desafio.categorias.models import CategoriaModel
+from Aulas.APIs.FastAPI.Desafio.atleta.models import AtletaModel
+from Aulas.APIs.FastAPI.Desafio.centro_treinamento.models import CentroTreinamentoModel
+
