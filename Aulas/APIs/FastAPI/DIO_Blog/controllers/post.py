52f1f7160a7a97439be07e1517d3fcc5c5c56b0c
Icaro Souza <icarosouza04@outlook.com>
2026-01-09 23:21:52 -0400

    Implementação final do CRUD

--- Aulas/APIs/FastAPI/DIO_Blog/controllers/post.py
+++ Aulas/APIs/FastAPI/DIO_Blog/controllers/post.py
@@ -1,24 +1,34 @@
 from fastapi import status, APIRouter
-from schemas.post import PostIn
+from Aulas.APIs.FastAPI.DIO_Blog.services.post import PostService
+from schemas.post import PostIn, PostUpdateIn
 from views.post import PostOut
 from Aulas.APIs.FastAPI.DIO_Blog.models.post import posts
 from Aulas.APIs.FastAPI.DIO_Blog.database import database
 
 router = APIRouter(prefix = "/posts")
 
+service = PostService()
+
 @router.get("/", response_model = list[PostOut])
 async def read_posts(published: bool, limit: int, skip: int = 0):
-    query = posts.select()
-    return await database.fetch_all(query)
+    return await service.read_all(published = published, limit = limit, skip = skip)
 
 
 @router.post("/", status_code = status.HTTP_201_CREATED, response_model = PostOut)
-def creat_post(post: PostIn):
-    comand = posts.inset().values(
-    title = post.title,
-    content = post.content, 
-    published_at = post.published_at
-    )
-    return post
+async def creat_post(post: PostIn):
+    return {**post.model_dump(), "id": await service.create(post)}
 
 
+@router.get("/{id}", response_model = PostOut)
+async def read_post(id: int):
+    return await service.read(id)
+
+
+@router.patch("/{id}", response_model = PostOut)
+async def update_post(id: int, post: PostUpdateIn):
+    return await service.update(id = id, post = post)
+    
+
+@router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT, response_model = None)
+async def delete_post(id: int):
+    await service.delete(id)
--- Aulas/APIs/FastAPI/DIO_Blog/views/post.py
+++ Aulas/APIs/FastAPI/DIO_Blog/views/post.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from pydantic import BaseModel
 
 class PostOut(BaseModel):
+    id: int
     tittle: str
     content: str
     published_at: datetime | None
-    date: datetime
